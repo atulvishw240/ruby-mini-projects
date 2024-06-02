1722c28b73bead0c030d12fec0b49d313ddbacbf
Atul Vishwakarma <atulvishw240@gmail.com>
2024-06-02 18:10:29 +0530

    Project complete

--- stock-picker/stockpicker.rb
+++ stock-picker/stockpicker.rb
@@ -5,26 +5,26 @@
 # Select 1st element of the array as buying price => 17
 # Find temp_selling_price from the rest of the array => (3, 6, 9, 15....)
 # Calculate temp_profit => temp_selling_price - temp_buying_price
-# If temp_profit is greater than profit then sell at that price, therefore update the selling price and profit
+# If temp_profit is greater than profit then sell at that price, therefore update buy_price, sell-price and Profit
 # Move to next element of the array => 3, 6, 9, 15, 8, 6, 1, 10
 # Selected 3 as temp buy_price
 # If for any combination with 3 and after 3 elements profit is greater than it was with 17
-# Update both buying price and selling price
+# Update buy_price and sell_price and profit
 # Keep doing it until you reach the end of array
 
 
 def stockpicker(array)
     buy_price = 0
     sell_price = 0
-    profit = -99999
+    profit = 0
 
     for i in array do
         temp_buy = i
 
         j = array.index(temp_buy) + 1
+        # j = 1 This led to the while loop bug. During second iteration (1 > 1) which evaluated to false
         while j > array.index(temp_buy) && j < array.size
             temp_sell = array[j]
-            puts temp_buy
             temp_profit = temp_sell - temp_buy
             if temp_profit > profit
                 profit = temp_profit
@@ -41,7 +41,7 @@ def stockpicker(array)
     return [buy_day, sell_day]
 end
 
-array = [17, 3, 6, 9, 15, 8, 6, 1, 10]
+array = [17, 3, 6, 9, 2, 15, 8, 6, 20, 1, 10]
 p stockpicker(array)
 
 
