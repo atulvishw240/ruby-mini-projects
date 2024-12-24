0e263b8cc4dc73ea45d308c635154bd45e287a86
Atul Vishwakarma <atulvishw240@gmail.com>
2024-12-24 15:25:28 +0530

    Refactor #stockpicker

--- lib/stockpicker.rb
+++ lib/stockpicker.rb
@@ -1,29 +1,25 @@
 # Return best day to buy and the best day to sell => [buy, sell]
 def stockpicker(array)
-    buy_price = 0
-    sell_price = 0
-    profit = 0
+	buy = 0
+	sell = 0
+	profit = 0
 
-    for i in array do
-        temp_buy = i
+	array.each_with_index do |temp_buy, index|
+		j = index + 1
+		while j < array.size
+			temp_sell = array[j]
+			temp_profit = temp_sell - temp_buy
+			if temp_profit > profit
+				profit = temp_profit
+				buy = index
+				sell = j
+			end
+			
+			j += 1
+		end
+	end
 
-        j = array.index(temp_buy) + 1
-        while j > array.index(temp_buy) && j < array.size
-            temp_sell = array[j]
-            temp_profit = temp_sell - temp_buy
-            if temp_profit > profit
-                profit = temp_profit
-                buy_price = temp_buy
-                sell_price = temp_sell
-            end
-            
-            j += 1
-        end
-    end
-
-    buy_day = array.index(buy_price)
-    sell_day = array.index(sell_price)
-    return [buy_day, sell_day]
+	[buy, sell]
 end
 
 
